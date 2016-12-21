bb6015234762723339c90fecb368e9fad1e9feea
Ivanna <ivanna.nagirna@gmail.com>
2016-12-21 17:09:50 +0200

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat');
+	htmlmin = require('gulp-htmlmin');
 
 var env,
 	coffeeSources,
@@ -70,7 +71,7 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
@@ -83,7 +84,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+		.pipe(gulpif (env === 'production', htmlmin({collapseWhitespace: true})))
+		.pipe(gulpif (env === 'production', gulp.dest(outputDir)))
 		.pipe(connect.reload())
 });
 
